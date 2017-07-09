62779aea32744ca448360c6982618a893538f31b
pavi97 <pavisaravanan97@gmail.com>
2017-07-09 08:34:02 +0530

    Update second.java

--- second.java
+++ second.java
@@ -1,1 +1,20 @@
+import java.util.*;
+public class hello
+{
+ public static void main(String args[])
+  {
+    public int x;
+    Scanner in=new Scanner(System.in);
+    x=in.nextInt();
+    if(x%2==0)
+    {
+          System.out.println("Even");
+    }
+    else 
+    {
+          System.out.println("Odd");
+    }
+      
 
+  }
+}
