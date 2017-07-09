95e630c513753a2108dd84240ee096833bbd49f6
pavi97 <pavisaravanan97@gmail.com>
2017-07-09 08:30:27 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class hello
+{
+ public static void main(String args[])
+  {
+    public int x;
+    Scanner in=new Scanner(System.in);
+    x=in.nextInt();
+    if(x>0)
+    {
+          System.out.println("Positive");
+    }
+    else if(x<0)
+    {
+          System.out.println("Negative");
+         
+    }
+    else
+    {
+      System.out.println("Zero");
+      }
+      
+
+  }
+}
